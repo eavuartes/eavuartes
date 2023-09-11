19573ecb7a57a6992e6b5a6c8a259b7c4367dc21
eav <112521859+eavuartes@users.noreply.github.com>
2023-09-11 12:08:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,165 +1,317 @@
+const root = document.documentElement;
+const dropdownTitleIcon = document.querySelector(".dropdown-title-icon");
+const dropdownTitle = document.querySelector(".dropdown-title");
+const dropdownList = document.querySelector(".dropdown-list");
+const mainButton = document.querySelector(".main-button");
+const floatingIcon = document.querySelector(".floating-icon");
 
-let ubicacionPrincipal  = window.pageYOffset;
-window.onscroll = function() {
-    let Desplazamiento_Actual = window.pageYOffset;
-    if(ubicacionPrincipal >= Desplazamiento_Actual){
-        document.getElementById('navbar').style.top = '15px';
-    }
-    else{
-        document.getElementById('navbar').style.top = '-100px';
-    }
-    ubicacionPrincipal = Desplazamiento_Actual;
+const icons = {
+  linkedin:
+    "M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z",
+  instagram:
+    "M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z",
+  facebook:
+    "M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z",
+  twitter:
+    "M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z",
+  youtube:
+    "M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"
+};
+
+const listItems = ["Linkedin", "Instagram", "Facebook", "Twitter", "Youtube"];
+
+const iconTemplate = (path) => {
+  return `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
+      <path d="${path}" />
+    </svg>
+  `;
+};
+
+const listItemTemplate = (text, translateValue) => {
+  return `
+    <li class="dropdown-list-item">
+      <button class="dropdown-button list-button" data-translate-value="${translateValue}%">
+        <span class="text-truncate">${text}</span>
+      </button>
+    </li>
+  `;
+};
+
+const renderListItems = () => {
+  dropdownList.innerHTML += listItems
+    .map((item, index) => {
+      return listItemTemplate(item, 100 * index);
+    })
+    .join("");
+};
+
+window.addEventListener("load", () => {
+  renderListItems();
+});
+
+const setDropdownProps = (deg, ht, opacity) => {
+  root.style.setProperty("--rotate-arrow", deg !== 0 ? deg + "deg" : 0);
+  root.style.setProperty("--dropdown-height", ht !== 0 ? ht + "rem" : 0);
+  root.style.setProperty("--list-opacity", opacity);
+};
+
+mainButton.addEventListener("click", () => {
+  const listWrapperSizes = 3.5; // margins, paddings & borders
+  const dropdownOpenHeight = 4.6 * listItems.length + listWrapperSizes;
+  const currDropdownHeight =
+    root.style.getPropertyValue("--dropdown-height") || "0";
+
+  currDropdownHeight === "0"
+    ? setDropdownProps(180, dropdownOpenHeight, 1)
+    : setDropdownProps(0, 0, 0);
+});
+
+dropdownList.addEventListener("mouseover", (e) => {
+  const translateValue = e.target.dataset.translateValue;
+  root.style.setProperty("--translate-value", translateValue);
+});
+
+dropdownList.addEventListener("click", (e) => {
+  const clickedItemText = e.target.innerText.toLowerCase().trim();
+  const clickedItemIcon = icons[clickedItemText];
+
+  dropdownTitleIcon.innerHTML = iconTemplate(clickedItemIcon);
+  dropdownTitle.innerHTML = clickedItemText;
+  setDropdownProps(0, 0, 0);
+});
+
+dropdownList.addEventListener("mousemove", (e) => {
+  const iconSize = root.style.getPropertyValue("--floating-icon-size") || 0;
+  const x = e.clientX - dropdownList.getBoundingClientRect().x;
+  const y = e.clientY - dropdownList.getBoundingClientRect().y;
+  const targetText = e.target.innerText.toLowerCase().trim();
+  const hoverItemText = icons[targetText];
+
+  floatingIcon.innerHTML = iconTemplate(hoverItemText);
+  root.style.setProperty("--floating-icon-left", x - iconSize / 2 + "px");
+  root.style.setProperty("--floating-icon-top", y - iconSize / 2 + "px");
+});
+
+const dropdownBtn = document.querySelectorAll(".dropdown-btn");
+const dropdown = document.querySelectorAll(".dropdown");
+const hamburgerBtn = document.getElementById("hamburger");
+const navMenu = document.querySelector(".menu");
+const links = document.querySelectorAll(".dropdown a");
+
+function setAriaExpandedFalse() {
+  dropdownBtn.forEach((btn) => btn.setAttribute("aria-expanded", "false"));
 }
 
+function closeDropdownMenu() {
+  dropdown.forEach((drop) => {
+    drop.classList.remove("active");
+    drop.addEventListener("click", (e) => e.stopPropagation());
+  });
+}
+
+function toggleHamburger() {
+  navMenu.classList.toggle("show");
+}
+
+dropdownBtn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    const dropdownIndex = e.currentTarget.dataset.dropdown;
+    const dropdownElement = document.getElementById(dropdownIndex);
+
+    dropdownElement.classList.toggle("active");
+    dropdown.forEach((drop) => {
+      if (drop.id !== btn.dataset["dropdown"]) {
+        drop.classList.remove("active");
+      }
+    });
+    e.stopPropagation();
+    btn.setAttribute(
+      "aria-expanded",
+      btn.getAttribute("aria-expanded") === "false" ? "true" : "false"
+    );
+  });
+});
+
+// close dropdown menu when the dropdown links are clicked
+links.forEach((link) =>
+  link.addEventListener("click", () => {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+    toggleHamburger();
+  })
+);
+
+// close dropdown menu when you click on the document body
+document.documentElement.addEventListener("click", () => {
+  closeDropdownMenu();
+  setAriaExpandedFalse();
+});
+
+// close dropdown when the escape key is pressed
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    closeDropdownMenu();
+    setAriaExpandedFalse();
+  }
+});
+
+// toggle hamburger menu
+hamburgerBtn.addEventListener("click", toggleHamburger);
 
 
-//The menu js class:
-class sikFloatingMenu {
-    menuEl = null;
-    constructor(_menu) { 
-        //The menu element: 
-        this.menuEl = typeof _menu === 'string'
-                      ? document.querySelector(_menu)
-                      : _menu;
-        //Attach handlers:
-        this.attachHandlers();
-    }
-    attachHandlers() {
-        if (this.menuEl) {
-            this._on(this.menuEl, 'click', '.trigger-menu', this._handler.bind(this));
-        }
-    }
-    _open(item) {
-        let opened = item.closest('.fmenu').querySelectorAll('.trigger-menu.open');
-        for (const ele of opened) {
-            this._close(ele);
-        }
-        item.classList.add('open');
-        //expand:
-        let list = item.closest('li').querySelector(".floating-menu");
-        list.style.setProperty("max-height", this._measureExpandableList(list));
-        list.style.setProperty("opacity", "1");
-        item.style.setProperty("max-width", this._measureExpandableTrigger(item));
-    }
-    _close(item) {
-        let list = item.closest('li').querySelector(".floating-menu");
-        item.classList.remove('open');
-        //shrink:
-        list.style.removeProperty("max-height");
-        list.style.removeProperty("opacity");
-        item.style.removeProperty("max-width");
-    }
-    _measureExpandableList(list) {
-        const items = list.querySelectorAll('li');
-        return (items.length * this._getHeight(items[0], "outer") + 10) + 'px';
-    }
-    _measureExpandableTrigger(item) {
-        const textEle = item.querySelector('.text');
-        const sizeBase = this._getWidth(item, "outer");
-        const sizeExpandLabel = this._getWidth(textEle, "outer");
-        return (sizeBase + sizeExpandLabel + 6) + 'px';
-    }
-    _handler(el, ev) {
-        if (el.classList.contains('open')) {
-            this._close(el);
+
+
+
+
+
+const allVideos = document.querySelectorAll(".video");
+
+allVideos.forEach((v) => {
+ v.addEventListener("mouseover", () => {
+  const video = v.querySelector("video");
+  video.play();
+ });
+ v.addEventListener("mouseleave", () => {
+  const video = v.querySelector("video");
+  video.pause();
+ });
+});
+
+$(function () {
+ $(".logo, .logo-expand, .discover").on("click", function (e) {
+  $(".main-container").removeClass("show");
+  $(".main-container").scrollTop(0);
+ });
+ $(".trending, .video").on("click", function (e) {
+  $(".main-container").addClass("show");
+  $(".main-container").scrollTop(0);
+  $(".sidebar-link").removeClass("is-active");
+  $(".trending").addClass("is-active");
+ });
+
+ $(".video").click(function () {
+  var source = $(this).find("source").attr("src");
+  var title = $(this).find(".video-name").text();
+  var person = $(this).find(".video-by").text();
+  var img = $(this).find(".author-img").attr("src");
+  $(".video-stream video").stop();
+  $(".video-stream source").attr("src", source);
+  $(".video-stream video").load();
+  $(".video-p-title").text(title);
+  $(".video-p-name").text(person);
+  $(".video-detail .author-img").attr("src", img);
+ });
+});  
+
+
+
+
+// Setting up variables
+const $studentList = $('.student-list').children();
+$('.student-list').prepend('<div class="notFound"></div>');
+$('.notFound').html(`<span>No results</span>`);
+$('.notFound').hide();
+
+// Bulding a list of ten students and displaying them on the page
+function showPage(studentList, pageNum = 1){
+    const showPerPage = 10;    
+    // hide all students on the page
+    $(studentList).hide(); 
+    
+    // Get start/end for each student based on the page number
+    const calcStart = (pageNum) => pageNum === 1 ? 0 : (pageNum - 1) * showPerPage;
+    const start = calcStart(pageNum);
+    const end = pageNum * showPerPage;
+
+    // Looping through all students in studentList
+    $(studentList).slice(start,end).each(function(i, li){
+        // if student should be on this page number
+        // show the student
+        $(li).fadeIn(50);
+    });
+}
+
+// Search component
+const searchBar = `
+    <div class="student-search">
+        <input placeholder="Search for students...">
+        <button>Search</button>
+    </div>
+`;
+$('.page-header').append(searchBar);
+
+$('.student-search input').on('keyup', function(){
+    const searchQuery = $(this).val();
+    const searchResults = searchStudent($('.student-list li'), searchQuery.toUpperCase());
+    showPage(searchResults);
+    appendPageLinks(searchResults);
+});
+
+
+// Student search
+function searchStudent(element, filter){
+
+    $(element).each(function(){         
+        if($(this).text().toUpperCase().includes(filter)){
+            $(this).show();
         } else {
-            this._open(el);
+            $(this).hide();
+        }        
+    });
+    let num = $('.student-item:not([style*="display: none"])').length;
+    let searchRes = $('.student-item:not([style*="display: none"])');
+    num > 0 ? $('.notFound').hide() : $('.notFound').show();
+    return searchRes;
+};
+
+
+
+// Creating all page links based on a list of students
+function appendPageLinks(studentList){
+    // determine how many pages for this student list
+    totalPageNum = Math.ceil(studentList.length / 10);
+    // create a page link section
+    const pagination = 'pagination';
+    // add a page link to the page link section
+    // if class the element already exists
+    if($('.pagination').length === 0){
+        $('.page').append(`
+        <div class="${pagination}">
+            <ul></ul>
+        </div>
+    `);
+    } 
+ 
+    // remove the old page link section from the site
+    $('.pagination ul').children().remove();
+
+    if (totalPageNum > 1){
+        // 'for' every page
+        for(let i=0; i<totalPageNum; i++){
+            const pageLink = `
+                <li>
+                    <a href="#">${i + 1}</a>
+                </li>
+            `;
+            // append our new page link section to the site
+            $('.pagination ul').append(pageLink);
         }
     }
-    _on(ele, type, selector, handler) {
-        ele.addEventListener(type, function(ev) {
-            let el = ev.target.closest(selector);
-            if (el) handler.call(this, el, ev); //The element is bind to this
+
+    $('.pagination ul li').children().first().addClass('active'); 
+        
+        // define what happens when you click a link (event listener)
+        $('.pagination ul').on('click', 'a', function(e){
+            e.preventDefault();
+            const pgNum = parseInt($(e.target).text());
+            // Use showPage() to display the page for the link clicked
+            showPage(studentList, pgNum);
+            // mark that link as 'active'
+            $(this).parent().siblings().children().removeClass('active');
+            $(this).addClass('active');
         });
-    }
-    _getWidth(el, type) {
-        if (type === 'inner') 
-            return el.clientWidth;
-        else if (type === 'outer') 
-            return el.offsetWidth;
-        return 0;
-    }
-    _getHeight(el, type) {
-        if (type === 'inner')
-            return el.clientHeight;
-        else if (type === 'outer')
-            return el.offsetHeight;
-        return 0;
-    }
-}
+} 
 
-//Intialize menu: 
-window.sik_menu = new sikFloatingMenu("#mymenu");
-
-
-
-
-
-const quickViewButtons = document.querySelectorAll('[data-quick-view]');
-const closeButtons = document.querySelectorAll('[data-close]');
-const fullwidthCards = document.querySelectorAll('.fullwidth');
-let toggle; // Quick view <button>.
-let toggleParent; // <li>.
-let fullwidth; // Fullwidth card to be "injected".
-
-const openQuickView = (toggle, toggleParent, fullwidth) => {
-    toggle.setAttribute('aria-expanded', 'true');
-    toggleParent.classList.toggle('is-selected');
-    fullwidth.classList.toggle('is-hidden');
-    // Make fullwidth card keyboard focusable. 
-    fullwidth.setAttribute('tabIndex', '0');
-};
-
-const closeQuickView = (toggle, toggleParent, fullwidth) => {
-    toggle.setAttribute('aria-expanded', 'false');
-    toggleParent.classList.toggle('is-selected');
-    fullwidth.classList.toggle('is-hidden');
-    fullwidth.removeAttribute('tabIndex');
-};
-
-quickViewButtons.forEach(quickView => {
-    // Add appropriate ARIA attributes for "toggle" behaviour.
-    fullwidth = quickView.parentElement.nextElementSibling;
-    quickView.setAttribute('aria-expanded', 'false');
-    quickView.setAttribute('aria-controls', fullwidth.id);
-
-    quickView.addEventListener('click', (e) => {
-        toggle = e.target;
-        toggleParent = toggle.parentElement;
-        fullwidth = toggleParent.nextElementSibling;
-
-        // Open (or close) fullwidth card.
-        if (toggle.getAttribute('aria-expanded') === 'false') {
-            // Do we have another fullwidth card already open? If so, close it.
-            fullwidthCards.forEach(fullwidthOpen => {
-                if (!fullwidthOpen.classList.contains('is-hidden')) {
-                    toggleParentOpen =
-                        fullwidthOpen.previousElementSibling;
-                    toggleOpen = toggleParentOpen.querySelector(
-                        '[data-quick-view]'
-                    );
-
-                    closeQuickView(toggleOpen, toggleParentOpen, fullwidthOpen);
-                }
-            });
-
-            openQuickView(toggle, toggleParent, fullwidth);
-        } else {
-            closeQuickView(toggle, toggleParent, fullwidth);
-        }
-    });
-});
-
-closeButtons.forEach(close => {
-    close.addEventListener('click', (e) => {
-        fullwidth = e.target.parentElement;
-        toggleParent = e.target.parentElement.previousElementSibling;
-        toggle = toggleParent.querySelector('[data-quick-view]');
-
-        closeQuickView(toggle, toggleParent, fullwidth);
-        toggle.focus(); // Return keyboard focus to "toggle" button.
-    });
-});
-
-
-feather.replace()
+showPage($studentList);
+appendPageLinks($studentList);
